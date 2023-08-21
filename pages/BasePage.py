350af87b1ee5f125e513b9a5dce3b0fb23adc46a
unknown <oxana.udodova@mail.ru>
2023-08-21 14:26:44 +0400

    добавила пустую строку между функциями,выполнила задание по путям

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -1,7 +1,9 @@
 class BasePage:
     def _init_(self):
         pass
+
     def findElement(self):
         pass
+
     def findElements(self):
         pass
---
+++ pages/LoginPage.py
@@ -0,0 +1,16 @@
+class LoginPageLocators:
+    LOGIN_FIELD = '//*[@id="field_email"]'
+    PASSWORD_FIELD = '//*[@id="field_password"]'
+    ENTER_BUTTON = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/input'
+    QR_LINK = '//*[@id="qrCode-4047394996"]'
+    REGISTER_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/a'
+    VK_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[1]'
+    MAIL_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[2]'
+    GOOGLE_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[3]'
+    YANDEX_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[4]'
+    APPLE_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[5]'
+    ENTER_TAB_LINK = '//*[@id="login-4047394950"]'
+    QR_TAB_LINK = '//*[@id="qrCode-4047394996"]'
+
+
+
