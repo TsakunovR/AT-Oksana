e9b417b117abe628c4bd2d12c7e42c113787cb4a
unknown <oxana.udodova@mail.ru>
2023-08-29 19:21:39 +0400

    переписала пути с использованием data-l

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,16 +1,13 @@
 class LoginPageLocators:
     LOGIN_FIELD = '//*[@id="field_email"]'
     PASSWORD_FIELD = '//*[@id="field_password"]'
-    ENTER_BUTTON = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/input'
-    QR_LINK = '//*[@id="qrCode-4047394996"]'
-    REGISTER_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/a'
-    VK_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[1]'
-    MAIL_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[2]'
-    GOOGLE_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[3]'
-    YANDEX_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[4]'
-    APPLE_LINK = '//*[@id="tabpanel-login-4047394950"]/form/div[4]/div[2]/div[2]/a[5]'
-    ENTER_TAB_LINK = '//*[@id="login-4047394950"]'
-    QR_TAB_LINK = '//*[@id="qrCode-4047394996"]'
-
-
-
+    ENTER_BUTTON = '//*[@data-l="t,sign_in"]'
+    QR_LINK = '//*["@data-l="t,get_qr"]'
+    REGISTER_LINK = '//*[@data-l="t,register"]'
+    VK_LINK = '//*[@data-l="t,vkc"]'
+    MAIL_LINK = '//*[@data-l="t,mailru"]'
+    GOOGLE_LINK = '//*[@data-l="t,google"]'
+    YANDEX_LINK = '//*[data-l="t,yandex"]'
+    APPLE_LINK = '//*[@data-l="t,apple"]'
+    ENTER_TAB_LINK = '//*[@data-l="t,login_tab"]'
+    QR_TAB_LINK = '//*[@data-l="t,qr_tab"]'
